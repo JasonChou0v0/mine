58aa0036af7fe07f9aa08ace49c1825e9abd5759
JasonChou <76592626+JasonChou0v0@users.noreply.github.com>
2024-09-22 15:56:12 +0800

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
   title: 'Jason 碎碎念 | 开发者、细节控',
   description:
-    '我叫 Jason，我热爱开发，创新，享受生活，以及在未知领域中探索。',
+    'feedId:60563448988313600+userId:42885649245851648',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://voe.cool'
